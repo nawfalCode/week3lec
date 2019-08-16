8f3303104a9abb0e267280109b628f4b41677290
Nawfal Ali <nawfalrezo@gmail.com>
2019-08-16 12:25:41 +1000

    calc

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.get("/wiki/:keyword/fit12095/:id",function(req,res){
     res.send("Hello to my first Express App!!!");
 });
 
+app.get("/report",function (req,res) {
+    console.log(req.url);
+    console.log(req.query);
+
+    console.log("the name is==="+req.query.name);
+    console.log("the age is==="+req.query.age);
+    
+    
+    res.send("This is the report page!!!");
+});
 app.get("/about",function (req,res) {
     res.send("This is the about page!!!");
 });
