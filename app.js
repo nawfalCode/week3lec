17c88b244b892a39f8e329392db6abc50501cb39
Nawfal Ali <nawfalrezo@gmail.com>
2019-08-16 11:48:01 +1000

    add wiki point

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ let app=express();
 app.get("/",function(req,res){
     res.send("Hello to my first Express App!!!");
 });
+app.get("/wiki/:keyword/fit12095/:id",function(req,res){
+    console.log(req.url);
+    console.log(req.params);
+    
+    res.send("Hello to my first Express App!!!");
+});
 
 app.get("/about",function (req,res) {
     res.send("This is the about page!!!");
